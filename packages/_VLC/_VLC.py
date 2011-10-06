24f186d7d410a7f9179a964229492d3ad6d843b5
Kyle Levien <tehkyle@gmail.com>
2011-10-06 14:41:12 -0700

    Added VLC package.
    Signed-off-by: Kyle Levien <tehkyle@gmail.com>

---
+++ packages/_VLC/_VLC.py
@@ -0,0 +1,17 @@
+'''
+VLC package class
+
+Created on Oct 6, 2011
+
+@author: levienk
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _VLC(Package):
+
+
+    def __init__(self):
+        Package.__init__(self, "\\".join(__file__.split("\\")[:-3]))
+        
