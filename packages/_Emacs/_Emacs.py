1eab3753bf904938bbbab978a6cacd6c82eb22c4
Kyle Levien <tehkyle@gmail.com>
2011-10-06 14:48:18 -0700

    Added Emacs package.

---
+++ packages/_Emacs/_Emacs.py
@@ -0,0 +1,17 @@
+'''
+Emacs package class
+
+Created on Oct 6, 2011
+
+@author: levienk
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _Emacs(Package):
+
+
+    def __init__(self):
+        Package.__init__(self, "\\".join(__file__.split("\\")[:-3]))
+        
