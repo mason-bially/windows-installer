a49cd81a5aa6c4e1104a888a5bd13c103a872200
nycteaa <nycteaa@students.wwu.edu>
2011-10-17 00:18:06 -0700

    Inkscape package added
    Signed-off-by: nycteaa <nycteaa@students.wwu.edu>

---
+++ packages/_Inkscape/_Inkscape.py
@@ -0,0 +1,17 @@
+'''
+Inkscape package class
+
+Created on Oct 16, 2011
+
+@author: nycteaa
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _InkscapePackage):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
