f97c83b30895640d4cd1df3fc920432a85937ed6
Kyle Levien <tehkyle@gmail.com>
2011-10-18 21:34:23 -0700

    Added Adobe Reader Package NOTE: Download does not work on this one, Adobe hides the files. Will need a more advanced opinion.
    Signed-off-by: Kyle Levien <tehkyle@gmail.com>

---
+++ packages/_Adobe_Reader/_Adobe_Reader.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 23, 2011
+
+@author: bucherj
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _Adobe_Reader(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
