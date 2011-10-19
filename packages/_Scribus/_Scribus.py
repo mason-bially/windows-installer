c23bf0a382eeee84a806de754e4fa5484ecdbf30
Kyle Levien <tehkyle@gmail.com>
2011-10-18 22:35:42 -0700

    Added Scribus Package. Version and Download works. Will grab the latest stable version, attempted to grab latest maintained version, but conflicts with the RC# part of the version will need to be addressed in utils.py for that to work.
    Signed-off-by: Kyle Levien <tehkyle@gmail.com>

---
+++ packages/_Scribus/_Scribus.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 23, 2011
+
+@author: bucherj
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _Scribus(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
