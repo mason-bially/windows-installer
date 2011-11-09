30e7f181398c589fa19de2747c15232603d5e5b8
nycteaa <nycteaa@students.wwu.edu>
2011-11-09 10:30:43 -0800

    Adding VirtualBox and LibreOffice to the repository. Neither work for an error I am not able to figure out. error is AttributeError:'module' object has no attribute '_LibreOffice'
    Signed-off-by: nycteaa <nycteaa@students.wwu.edu>

---
+++ packages/_VirtualBox/__VirtualBox.py
@@ -0,0 +1,13 @@
+'''
+@author: Auresa Nyctea
+'''
+from ..defaultpackage.package import Package
+
+
+
+class __VirtualBox(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
