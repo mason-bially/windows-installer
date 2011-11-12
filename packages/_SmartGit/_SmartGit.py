4b89750c33b56cac67930087ed41d1c3d398d338
nycteaa <nycteaa@students.wwu.edu>
2011-11-12 15:20:59 -0800

    Neither download. Need to figure out how to work with flash/php code and accept terms.
    Signed-off-by: nycteaa <nycteaa@students.wwu.edu>

---
+++ packages/_SmartGit/_SmartGit.py
@@ -0,0 +1,13 @@
+'''
+@author: nycteaa
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _SmartGit(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
---
+++ packages/_TortoiseGit_x64/_TortoiseGit_x64.py
@@ -0,0 +1,13 @@
+'''
+@author: nycteaa
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _TortoiseGit_x64(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
