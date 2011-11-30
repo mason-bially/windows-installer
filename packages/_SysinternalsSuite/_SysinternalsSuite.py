6ef9d82adafb3249531dbdde86ee3dbeaa82b393
Kyle Levien <tehkyle@gmail.com>
2011-11-29 23:44:36 -0800

    Added Sysinternals Suite. Will do some more tomorrow (Wed 11/30)
    Signed-off-by: Kyle Levien <tehkyle@gmail.com>

---
+++ packages/_SysinternalsSuite/_SysinternalsSuite.py
@@ -0,0 +1,13 @@
+'''
+@author: KyleLevien
+'''
+from ..defaultpackage.package import Package
+
+
+
+class _SysinternalsSuite(Package):
+
+
+    def __init__(self):
+        Package.__init__(self)
+        
